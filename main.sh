717e30f02b75c869aa3490c603aad88a0e3e3bd2
kingely86 <elyess.brahim@gmailcom>
2023-11-16 09:12:00 +0100

    création nouvelle arbo

---
+++ main.sh
@@ -0,0 +1,7 @@
+echo "BONJOUR git"
+a=2
+echo "2+1=4?"
+echo $a puis $(($a+1)) ca fait 3
+echo "bonsoir"
+ls /etc/
+exit 1
---
+++ server.py
@@ -0,0 +1,5 @@
+print("test server")
+b=3
+for i in range(a):
+    print(b)
+python3 -m http.server
